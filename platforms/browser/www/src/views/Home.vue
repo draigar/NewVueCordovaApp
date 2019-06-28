0088ad94e84be8e5cea9e19d8d6d88fa53884492
Ben Francis <siscorule@gmail.com>
2019-06-28 17:19:30 +0100

    Initial commit

---
+++ platforms/browser/www/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <h1>Apache Cordova</h1>
+    <div id="deviceready" class="blink">
+      <p class="event listening">Connecting to Device</p>
+      <p class="event received">Device is Ready</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  mounted() {
+    app.receivedEvent("deviceready");
+  }
+};
+</script>
---
+++ www/src/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    
+    <router-view>
+      <Home/>
+    </router-view>
+  </div>
+</template>
+<script>
+import Home from "../views/Home";
+export default {
+  name: "App",
+  components: {
+    Home
+  }
+};
+</script>
